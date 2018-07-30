a7b19c82da4db6d4634f62e0107d73c6eff03517
Kevin Wahome <wahomekevin@gmail.com>
2018-07-30 09:54:35 +0000

    Added specific files to be cleaned by webpack.

--- webpack.common.js
+++ webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['public'], {
+    new CleanWebpackPlugin(['public/*.*'], {
       watch: true
     }),
     new MiniCssExtractPlugin({
